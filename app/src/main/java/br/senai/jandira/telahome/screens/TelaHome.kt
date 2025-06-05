060c8336f493fbe35853651bc6712ecf2648399f
HELOYSA VILELA SANTOS <24122832@CFP127ALUNO.LOCAL>
2025-06-05 17:15:18 -0300

    Feito a tela Home

---
+++ app/src/main/java/br/senai/jandira/telahome/screens/TelaHome.kt
@@ -0,0 +1,320 @@
+
+package br.senai.jandira.telahome.screens
+
+import androidx.compose.foundation.BorderStroke
+import br.senai.jandira.telahome.R
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun TelaHome(modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFFFF8E1))
+    ) {
+
+        val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+        val scope = rememberCoroutineScope()
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 8.dp),
+            verticalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.logo),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .size(100.dp)
+                )
+                Spacer(modifier = Modifier.weight(1f))
+
+                IconButton(onClick = {}) {
+                    Icon(
+                        imageVector = Icons.Default.AccountCircle,
+                        contentDescription = "Menu",
+                        tint = Color(color = 0xFF4E342E),
+                        modifier = Modifier
+                            .size(80.dp)
+                    )
+                }
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 35.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            )
+            {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Card(
+                        modifier = Modifier
+                            .size(40.dp),
+                        shape = RoundedCornerShape(40.dp),
+                        colors = CardDefaults.cardColors(
+                            containerColor = Color(color = 0xFF4E342E)
+                        )
+                    ) {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxSize(),
+                            horizontalArrangement = Arrangement.Center,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.home),
+                                contentDescription = "",
+                                modifier = Modifier
+                                    .width(25.dp)
+                                    .height(25.dp)
+                            )
+                        }
+                    }
+
+                    Text(
+                        text = "Home",
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold,
+                        color = Color(color = 0xFF4E342E),
+                        modifier = Modifier
+                            .padding(top = 5.dp)
+                    )
+                }
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Card(
+                        modifier = Modifier
+                            .size(40.dp),
+                        shape = RoundedCornerShape(40.dp),
+                        colors = CardDefaults.cardColors(
+                            containerColor = Color(color = 0xFF4E342E)
+                        )
+                    ) {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxSize(),
+                            horizontalArrangement = Arrangement.Center,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.dog),
+                                contentDescription = "",
+                                modifier = Modifier
+                                    .width(25.dp)
+                                    .height(25.dp)
+                            )
+                        }
+                    }
+
+                    Text(
+                        text = "Pedidos",
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold,
+                        color = Color(color = 0xFF4E342E),
+                        modifier = Modifier
+                            .padding(top = 5.dp)
+                    )
+                }
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Card(
+                        modifier = Modifier
+                            .size(40.dp),
+                        shape = RoundedCornerShape(40.dp),
+                        colors = CardDefaults.cardColors(
+                            containerColor = Color(color = 0xFF4E342E)
+                        )
+                    ) {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxSize(),
+                            horizontalArrangement = Arrangement.Center,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.comentarios),
+                                contentDescription = "",
+                                modifier = Modifier
+                                    .width(25.dp)
+                                    .height(25.dp)
+                            )
+                        }
+                    }
+
+                    Text(
+                        text = "Feedback",
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold,
+                        color = Color(color = 0xFF4E342E),
+                        modifier = Modifier
+                            .padding(top = 5.dp)
+                    )
+                }
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Card(
+                        modifier = Modifier
+                            .size(40.dp),
+                        shape = RoundedCornerShape(40.dp),
+                        colors = CardDefaults.cardColors(
+                            containerColor = Color(color = 0xFF4E342E)
+                        )
+                    ) {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxSize(),
+                            horizontalArrangement = Arrangement.Center,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.form),
+                                contentDescription = "",
+                                modifier = Modifier
+                                    .width(25.dp)
+                                    .height(25.dp)
+                            )
+                        }
+                    }
+
+                    Text(
+                        text = "Sobre nós",
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold,
+                        color = Color(color = 0xFF4E342E),
+                        modifier = Modifier
+                            .padding(top = 5.dp)
+                    )
+                }
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 35.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Um pet adotado - uma família feliz",
+                    fontSize = 38.sp,
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center
+                )
+                Image(
+                    painter = painterResource(R.drawable.pet2),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .padding(top = 20.dp)
+                        .size(230.dp)
+                        .padding(bottom = 15.dp)
+                )
+                Text(
+                    text = "Na nossa ONG, acreditamos que todo animal merece um lar cheio de carinho. Resgatamos cães e gatos em situação de abandono, garantindo que recebam cuidados essenciais: vacinas, consultas veterinárias, castração e tratamento para doenças e ferimentos.\n" +
+                            "\n" +
+                            "Adotar transforma vidas — a do animal e a da família que o acolhe.",
+                    fontSize = 14.sp,
+                    color = Color.Black,
+                    fontWeight = FontWeight.Normal,
+                    textAlign = TextAlign.Justify
+                )
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 35.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+
+                Button(
+                    onClick = {},
+                    shape = RoundedCornerShape(25.dp),
+                    modifier = Modifier
+                        .width(170.dp)
+                        .height(50.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0xFF4E342E)
+                    )
+                ) {
+                    Text(
+                        text = "CADASTRAR ANIMAIS",
+                        color = Color.White,
+                        fontSize = 13.sp,
+                        textAlign = TextAlign.Center,
+
+                    )
+                }
+
+                Button(
+                    onClick = {},
+                    shape = RoundedCornerShape(25.dp),
+                    modifier = Modifier
+                        .width(170.dp)
+                        .height(50.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0xFF4E342E)
+                    )
+                ) {
+                    Text(
+                        text = "ANIMAIS CADASTRADOS",
+                        color = Color.White,
+                        fontSize = 13.sp,
+                        textAlign = TextAlign.Center,
+                        )
+                }
+
+            }
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun TelaHomePreview() {
+    TelaHome()
+}
